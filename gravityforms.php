ee36dcca962c15ac0a9eb6b00b524b7439008885
Bas Middelham <bas@middelham.nl>
2020-04-23 19:04:03 +0200

    Update gravityforms.php

--- gravityforms.php
+++ gravityforms.php
@@ -12,20 +12,6 @@
 namespace App;
 
 if ( class_exists( 'GFCommon' ) ) {
-
-	/**
-	 * GF: Iban validation
-	 */
-	add_filter( 'gform_field_validation', function( $result, $value, $form, $field ) {
-		if ( $field->cssClass === 'verify-iban' ) { // phpcs:ignore
-			if ( ! preg_match( '/[a-zA-Z]{2}[0-9]{2}[a-zA-Z0-9]{4}[0-9]{7}([a-zA-Z0-9]?){0,16}/', $value ) ) {
-				$result['is_valid'] = false;
-				$result['message']  = 'Invalid IBAN';
-			}
-		}
-		return $result;
-	}, 10, 4 );
-
 	/**
 	 * Disable Gravity Forms CSS.
 	 */
